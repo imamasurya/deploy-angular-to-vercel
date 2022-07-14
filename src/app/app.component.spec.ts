2c9fc603ea20e96776e9fe75c98de02ab6ec62e2
Imam A. Surya <imamas64@gmail.com>
2022-07-14 14:40:51 +0700

    fix: update app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('my-app');
   });
 });
